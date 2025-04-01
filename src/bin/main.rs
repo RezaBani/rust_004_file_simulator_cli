e06622303c2c1d5add95355c69eac0d00c5376a3
RezaBanivakil <rezabaniv@gmail.com>
2025-04-01 22:58:55 +0330

    args count and port validation separated from rest of args, true multi-threaded implemented. connection_id removed

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,7 +2,6 @@ use std::{
     env, fs,
     io::Write,
     net::{TcpListener, TcpStream},
-    sync::{Arc, RwLock},
     thread,
     time::Duration,
     usize,
@@ -11,47 +10,47 @@ use std::{
 use threadpool::ThreadPool;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let args = Arc::new(RwLock::new(read_args(args)));
     let pool = ThreadPool::new(4);
-    let connection_id = Arc::new(RwLock::new(0));
-    let port = (*args.read().unwrap()).port;
+    let port = read_port();
     let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        let connection_id = Arc::clone(&connection_id);
-        let args = Arc::clone(&args);
         pool.execute(move || {
-            {
-                (*connection_id.write().unwrap()) += 1;
-            }
-            write_to_stream(
-                stream,
-                connection_id.read().unwrap().clone(),
-                args.read().unwrap().clone(),
-            );
+            write_to_stream(stream);
             println!("Data Transmition Finished");
         });
     }
 }
 
-#[derive(Clone)]
 struct CommandLineArguments {
     content: String,
     rate_chunk: usize,
     rate_time: u64,
-    port: u16,
     repeat: bool,
     max_bytes: Option<usize>,
 }
 
-fn read_args(args: Vec<String>) -> CommandLineArguments {
+fn validate_args() -> Vec<String> {
+    let args: Vec<String> = env::args().collect();
     const MIN_ARG_COUNT: usize = 4;
     const MAX_ARG_COUNT: usize = 6;
     if args.len() < MIN_ARG_COUNT + 1 || args.len() > MAX_ARG_COUNT + 1 {
         panic!("Should have at least {MIN_ARG_COUNT} arguments and most {MAX_ARG_COUNT} arguments:\n
                 filename [path] rate_chunk [bytes] rate_time [ms] port [0-65535] loop [true or false (Default: false)] max_bytes [bytes (Dfault:No Limit)]")
     }
+    args
+}
+
+fn read_port() -> u16 {
+    let args = validate_args();
+    let port = (&args[4])
+        .parse()
+        .expect(format!("Specified Port {} is not a number", &args[4]).as_str());
+    port
+}
+
+fn read_args() -> CommandLineArguments {
+    let args = validate_args();
     let filename = &args[1];
     let rate_chunk = (&args[2])
         .parse()
@@ -59,9 +58,6 @@ fn read_args(args: Vec<String>) -> CommandLineArguments {
     let rate_time = (&args[3])
         .parse()
         .expect(format!("Specified Rate Time {} is not a number", &args[3]).as_str());
-    let port = (&args[4])
-        .parse()
-        .expect(format!("Specified Port {} is not a number", &args[4]).as_str());
     let repeat = if args.len() == 6 {
         if (&args[5]).to_lowercase() == "true" {
             true
@@ -89,13 +85,13 @@ fn read_args(args: Vec<String>) -> CommandLineArguments {
         content,
         rate_chunk,
         rate_time,
-        port,
         repeat,
         max_bytes,
     }
 }
 
-fn write_to_stream(mut stream: TcpStream, connection_id: u32, args: CommandLineArguments) {
+fn write_to_stream(mut stream: TcpStream) {
+    let args = read_args();
     loop {
         let mut pos = 0;
         let up_limit = if args.max_bytes.is_some_and(|n| n < args.content.len()) {
@@ -105,7 +101,7 @@ fn write_to_stream(mut stream: TcpStream, connection_id: u32, args: CommandLineA
         };
         while pos + args.rate_chunk < up_limit {
             let progress = 100.0 * (pos as f64 / up_limit as f64);
-            println!("Sending data to client #{connection_id} - Progress: {progress:.3}%");
+            println!("Sending data to client - Progress: {progress:.3}%");
             match stream.write(args.content[pos..pos + args.rate_chunk].as_bytes()) {
                 Ok(_) => stream.flush().expect("Error Flushing"),
                 Err(_) => println!(
@@ -116,7 +112,7 @@ fn write_to_stream(mut stream: TcpStream, connection_id: u32, args: CommandLineA
             pos += args.rate_chunk;
             thread::sleep(Duration::from_millis(args.rate_time));
         }
-        println!("Sending last chunk to client #{connection_id}");
+        println!("Sending last chunk to client");
         match stream.write(args.content[pos..].as_bytes()) {
             Ok(_) => stream.flush().expect("Error Flushing"),
             Err(_) => println!("Writing {} to stream failed", &args.content[pos..]),
